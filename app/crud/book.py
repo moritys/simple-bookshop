edf14003c2158260e2a9bd685ed525903fe7bfad
Maria Sharkova <moriko.tys@gmail.com>
2024-10-15 17:41:25 +0300

    добавлено исключение при попытке добавить существующую книгу

--- app/crud/book.py
+++ app/crud/book.py
@@ -1,11 +1,13 @@
+from typing import Optional
+
+from sqlalchemy import select
+
 from core.database import AsyncSessionLocal
 from models.book import Book
 from schemas.book import BookCreate
 
 
-async def create_book(
-    new_book: BookCreate
-) -> Book:
+async def create_book(new_book: BookCreate) -> Book:
     new_book_data = new_book.dict()
 
     db_book = Book(**new_book_data)
@@ -15,3 +17,14 @@ async def create_book(
         await session.commit()
         await session.refresh(db_book)
     return db_book
+
+
+async def get_book_id_by_name(book_name: str) -> Optional[int]:
+    async with AsyncSessionLocal() as session:
+        db_book_id = await session.execute(
+            select(Book.id).where(
+                Book.name == book_name
+            )
+        )
+        db_book_id = db_book_id.scalars().first()
+    return db_book_id
